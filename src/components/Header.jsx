a77f4ffc93afa08b8cc9d39848b6f6b5e83ad7dc
sachin sharma <harshit0808sharma@gmail.com>
2025-05-25 09:45:01 +0530

    fixed some error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,13 +14,49 @@ const Header = ({theam, setTheam}) => {
     <>
         <header className={`header ${theam ? "light-light": "light-dark"}`}>
             <a href="#" className="logo">Portfolio</a>
-            {/* <p id="menu-icon" onClick={()=> setOpen(!open)}>{open?<RxCross2/>:<IoMenu/>}</p> */}
+            
             <nav className="navbar">
-                <a className={color == 1? "active": ""} onClick={()=> setColor(1)} href="#home">Home</a>
-                <a className={color == 2? "active": ""} onClick={()=> setColor(2)} href="#about">About</a>
-                <a className={color == 3? "active": ""} onClick={()=> setColor(3)} href="#services">Skills</a>
-                <a className={color == 4? "active": ""} onClick={()=> setColor(4)} href="#projects">Projects</a>
-                <a className={color == 5? "active": ""} onClick={()=> setColor(5)} href="#contact">Contact</a>
+                <a className={color == 1? "active": ""} 
+                onClick={()=>{ 
+                  setColor(1); 
+                  const section =  document.getElementById("home");
+                  if(section){
+                    section.scrollIntoView({ behaviour: "smooth"});
+                    }
+                }}
+                >Home</a>
+                <a className={color == 2? "active": ""} 
+                  onClick={()=> {
+                    setColor(2);
+                    const section =  document.getElementById("about");
+                    if(section){
+                      section.scrollIntoView({ behaviour: "smooth"});
+                      }
+                    }}>About</a>
+                <a className={color == 3? "active": ""} 
+                  onClick={()=> {
+                    setColor(3);
+                    const section =  document.getElementById("services");
+                    if(section){
+                      section.scrollIntoView({ behaviour: "smooth"});
+                      }
+                    }}>Skills</a>
+                <a className={color == 4? "active": ""} 
+                  onClick={()=> {
+                    setColor(4);
+                    const section =  document.getElementById("projects");
+                    if(section){
+                      section.scrollIntoView({ behaviour: "smooth"});
+                      }
+                    }}>Projects</a>
+                <a className={color == 5? "active": ""} 
+                  onClick={()=> {
+                    setColor(5);
+                    const section =  document.getElementById("contact");
+                    if(section){
+                      section.scrollIntoView({ behaviour: "smooth"});
+                      }
+                    }}>Contact</a>
             </nav>
             <span className="theam-icon" onClick={()=> setTheam(!theam)}>{theam ? <MdNightlight/> : <MdLightMode />}</span>
         </header>
